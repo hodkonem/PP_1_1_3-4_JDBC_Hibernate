399c5639df728412fb7793aeb20e7ac21d291f23
Mikhail Latypov <hodkonem1984@gmail.com>
2023-08-30 20:28:05 +0300

    Исправил Main, сервис и проверил инкапсуляцию

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -3,7 +3,7 @@ package jm.task.core.jdbc.dao;
 import jm.task.core.jdbc.model.User;
 
 import java.util.List;
-
+// TODO: Реализовать этот класс в соответствии с заявленной функциональностью в будущих задачах
 public class UserDaoHibernateImpl implements UserDao {
     public UserDaoHibernateImpl() {
 
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -6,16 +6,14 @@ import java.sql.SQLException;
 
 public class Util {
     // реализуйте настройку соединения с БД
-    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
     private static final String URL = "jdbc:mysql://localhost:3306/mysqltest";
     private static final String USERNAME = "root";
     private static final String PASSWORD = "root";
 
     public static Connection getConnection() {
         try {
-            Class.forName(DRIVER);
             return DriverManager.getConnection(URL, USERNAME, PASSWORD);
-        } catch (ClassNotFoundException | SQLException e) {
+        } catch (SQLException e) {
             throw new RuntimeException(e);
         }
     }
